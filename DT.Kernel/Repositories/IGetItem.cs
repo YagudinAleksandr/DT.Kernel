f4e0fe43fa6956fb681afa8666d8550d1e4fb888
Александр <@DESKTOP-L9V170T>
2025-09-28 01:05:52 +0300

    Добавлены все репозитории

---
+++ DT.Kernel/Repositories/IGetItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DT.Kernel
+{
+    /// <summary>
+    /// Репозиторий получения записей
+    /// </summary>
+    /// <typeparam name="TAggregate">Агрегат</typeparam>
+    /// <typeparam name="TKey">Тип идентификатора</typeparam>
+    public interface IGetItem<TAggregate, TKey>
+         where TAggregate : class, IAggregateRoot where TKey : notnull 
+    {
+        /// <summary>
+        /// Получение записи по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <returns>Запись</returns>
+        TAggregate? GetById(TKey id);
+
+        /// <summary>
+        /// Получение записи по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <param name="cancellationToken">Токен отмены</param>
+        /// <returns>Запись</returns>
+        Task<TAggregate?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Получение всех записей
+        /// </summary>
+        /// <returns>Запрос типа <see cref="IQueryable"/></returns>
+        IQueryable<TAggregate> GetAll();
+
+        /// <summary>
+        /// Получение всех записей
+        /// </summary>
+        /// <returns>Запрос типа <see cref="IQueryable"/></returns>
+        Task<IQueryable<TAggregate>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ DT.Kernel/Repositories/IUpdateItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace DT.Kernel
+{
+    /// <summary>
+    /// Репозиторий обновления сущности
+    /// </summary>
+    /// <typeparam name="TAggregate">Агрегат</typeparam>
+    public interface IUpdateItem<TAggregate> 
+        where TAggregate : IAggregateRoot
+    {
+        /// <summary>
+        /// Обновление агрегата
+        /// </summary>
+        /// <param name="aggregate">Агрегат</param>
+        void Update(TAggregate aggregate);
+
+        /// <summary>
+        /// Обновление агрегата
+        /// </summary>
+        /// <param name="aggregate">Агрегат</param>
+        /// <param name="cancellationToken">Токен отмены</param>
+        Task UpdateAsync(TAggregate aggregate, CancellationToken cancellationToken = default);
+    }
+}
